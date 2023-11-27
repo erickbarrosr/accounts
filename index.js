464a228cd03978959fa9d13026f088a2dd03030c
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-26 22:52:55 -0300

    feat(index.js): get operations

--- index.js
+++ index.js
@@ -3,4 +3,28 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 
-console.log("Hello World!");
+getOperation();
+
+function getOperation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Realizar Depósito",
+          "Realizar Saque",
+          "Finalizar Operações",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
+
+      console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
