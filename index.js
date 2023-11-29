589b3f50fe40b953ef7d105b9bdb80a59e3c7bae
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-29 20:05:22 -0300

    feat: buildAccount

--- index.js
+++ index.js
@@ -24,7 +24,63 @@ function getOperation() {
     .then((answer) => {
       const action = answer["action"];
 
-      console.log(action);
+      if (action === "Criar Conta") {
+        createAccount();
+      }
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
+}
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir."));
+
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para a sua conta:",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdir("accounts", (err) => {
+          if (err) {
+            console.error("Error creating directory:", err);
+          } else {
+            console.log("Directory 'accounts' created successfully!");
+          }
+        });
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esta conta já existe, escolha outro nome!")
+        );
+
+        buildAccount();
+        return;
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green("Parabéns, a sua conta foi criada!"));
+
+      getOperation();
+    })
+    .catch((err) => console.error(err));
 }
